2ca4d05c74b7bdf43b00a5ee84beb71f0a2ef5e6
Shabana Mirza <shabanamirza2010@hotmail.com>
2019-01-16 14:53:34 +0000

    File pages set-up

---
+++ src/models/country.js
@@ -0,0 +1,18 @@
+const RequestHelper = require('../helpers/request_helper.js');
+const PubSub = require('../helpers/pub_sub.js');
+
+const Country = function () {
+  this.text = null;
+
+}
+
+Country.prototype.getData = function () {
+  const request = new RequestHelper('https://restcountries.eu/rest/v2/all');
+  request.get((data) =>
+{
+  this.text = data.country;
+  PubSub.publish('Country:country-loaded', this.text);
+});
+}
+
+module.exports = Country;
---
+++ src/views/select_view.js
@@ -0,0 +1,28 @@
+const PubSub = require('../helpers/pub_sub.js');
+
+const SelectView = function(element){
+ this.element = element;
+};
+
+SelectView.prototype.bindEvents = function(){
+ PubSub.subscribe('Country:all-countries-ready', (evt) => {
+   const allCountry = evt.detail;
+   this.populate(allCountry);
+ });
+
+ this.element.addEventListener('change', (evt) => {
+   const selectedIndex = evt.target.value;
+   PubSub.publish('SelectView:change', selectedIndex);
+ });
+};
+
+SelectView.prototype.populate = function(countryData){
+ countryData.forEach((country, index) => {
+   const option = document.createElement('option');
+   option.textContent = country.name;
+   option.value = index;
+   this.element.appendChild(option);
+ })
+}
+
+module.exports = SelectView;
